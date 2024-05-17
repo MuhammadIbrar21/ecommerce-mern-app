b582d441f9676c550a9b3d4ba93cade41535cc60
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2024-05-17 17:21:57 +0500

    updated db connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/dbcommerceapp')
-    .then(() => console.log('db connected successfully'))
-    .catch(() => console.log('db failed to connect!'))
+// mongoose.connect('mongodb://localhost:27017/dbcommerceapp')
+//     .then(() => console.log('db connected successfully'))
+//     .catch(() => console.log('db failed to connect!'))
+
+
+mongoose.connect('mongodb+srv://mribrar3344:mribrar3344@clusterforexpresspart3.hsnbfmb.mongodb.net/')
+    .then(() => console.log('db connection successful'))
+    .catch((err) => console.log(err))
