3d38af6b9fa3ccfffc3ce4d8ee43daa7e73bb818
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2024-05-17 18:07:07 +0500

    added db name

--- server/db/db.js
+++ server/db/db.js
@@ -5,6 +5,6 @@ const mongoose = require('mongoose');
 //     .catch(() => console.log('db failed to connect!'))
 
 
-mongoose.connect('mongodb+srv://mribrar3344:mribrar3344@clusterforexpresspart3.hsnbfmb.mongodb.net/')
+mongoose.connect('mongodb+srv://mribrar3344:mribrar3344@clusterforexpresspart3.hsnbfmb.mongodb.net/ecommercedb')
     .then(() => console.log('db connection successful'))
     .catch((err) => console.log(err))
