9d23c6dd8c09d988f5dd40c20002127d2385bdab
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2024-05-18 10:41:26 +0500

    added cors and path in app.js

--- server/App.js
+++ server/App.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const authRoutes = require('./routes/auth')
 const adminRoutes = require('./routes/admin')
+const cors = require('cors')
+const path = require('path')
 require('./db/db');
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3040;
 
+app.use(cors())
+
 app.use('/auth', authRoutes);
 
 app.use('/admin', adminRoutes);
 
+// app.use(express.static('./server/build'))
+
 app.use(express.static('./server/productImages'))
+
+app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "build")));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+})
 
 app.listen(port, () => {
     console.log(`Server is connected at port ${port}`);
